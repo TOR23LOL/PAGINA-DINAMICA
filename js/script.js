602792cbb03e50417dcfe05f44063da221cb9d34
JosephMina23 <josephmesias2000@gmail.com>
2024-11-05 11:26:29 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const $header = document.querySelector(".header");
+const $body = document.querySelector(".body");
+const $footer = document.querySelector(".footer");
+const $boton1 = document.querySelector("boton1");
+const $boton2 = document.querySelector("boton2");
